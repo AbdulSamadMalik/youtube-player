bc44ba0f132b2cce91b287892f976d1d310690c1
AbdulSamadMalik <samadmalik542762@gmail.com>
2021-06-22 16:15:32 +0530

    Update Header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,40 +1,64 @@
 import Hotkeys from '../../core/hotkeys';
 import { fromEvent, merge } from 'rxjs';
 import { $, areEqual, conditionalAttribute } from '../../core/utils';
+import { chooseFiles } from '../filepicker';
 import './header.css';
 
+// Refs
 const searchForm = $<HTMLFormElement>('#search-form'),
    searchInput = $<HTMLInputElement>('#search-input'),
-   searchContainer = $('.search-container');
+   searchContainer = $<HTMLDivElement>('.search-container'),
+   chooseFileButton = $<HTMLButtonElement>('#choose-file-button'),
+   headerHomeLink = $<HTMLAnchorElement>('.youtube-icon a');
 
-searchForm?.addEventListener('submit', (ev) => {
+// Methods
+const focusSearchBar = () => {
+   if (!areEqual(document.activeElement, searchInput)) {
+      searchInput.focus();
+   }
+};
+
+const blurSearchBar = () => {
+   if (areEqual(document.activeElement, searchInput)) {
+      searchInput.blur();
+   }
+};
+
+const handleSearchFormSubmit = (ev: Event) => {
    ev.preventDefault();
    if (searchInput && searchInput.value) {
       searchInput.blur();
       const query = searchInput.value.replace(/\s+/g, '+');
       window.open(`https://youtube.com/results?search_query=${query}`);
    }
-});
+};
 
-merge(
-   fromEvent<FocusEvent>(searchInput!, 'focus'),
-   fromEvent<FocusEvent>(searchInput!, 'blur')
-).subscribe(() => {
+const handleHomeLinkClick = (e: MouseEvent) => {
+   if (!e.shiftKey && !e.ctrlKey && !e.metaKey && !e.altKey) {
+      e.preventDefault();
+   }
+};
+
+const handleSearchBarFocus = () => {
    const searchBarFocused = areEqual(document.activeElement, searchInput);
    conditionalAttribute(searchContainer, searchBarFocused, 'has-focus', 'true');
+};
+
+// Event Listeners
+merge(
+   fromEvent<FocusEvent>(searchInput, 'focus'),
+   fromEvent<FocusEvent>(searchInput, 'blur')
+).subscribe(handleSearchBarFocus);
+
+headerHomeLink.addEventListener('click', handleHomeLinkClick);
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+chooseFileButton.addEventListener('click', chooseFiles);
+
+// Hotkeys
+Hotkeys.registerHotkey({
+   eventCode: 'KeyC',
+   handler: chooseFiles,
 });
-
-const focusSearchBar = () => {
-   if (!areEqual(document.activeElement, searchInput)) {
-      searchInput?.focus();
-   }
-};
-
-const blurSearchBar = () => {
-   if (areEqual(document.activeElement, searchInput)) {
-      searchInput?.blur();
-   }
-};
 
 Hotkeys.registerHotkey({
    eventCode: 'Slash',
