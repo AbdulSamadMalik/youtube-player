e562ab1a0198a0acc7951606a6e4be7fbc2118ac
AbdulSamadMalik <samadmalik542762@gmail.com>
2021-06-24 11:07:47 +0530

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,28 @@ import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
 const ViteConfig = defineConfig({
+   server: {
+      port: 4000,
+      open: true,
+      cors: false,
+      strictPort: true,
+   },
    plugins: [
       legacy({
          targets: ['defaults', 'IE 11'],
          polyfills: true,
+         modernPolyfills: true,
+         renderLegacyChunks: true,
       }),
    ],
    build: {
       target: 'es6',
       assetsDir: 'static',
       polyfillDynamicImport: true,
+      brotliSize: true,
+      chunkSizeWarningLimit: 400,
+      sourcemap: true,
+      write: true,
    },
 });
 
